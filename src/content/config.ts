502a455d66cb9381394df17060729e4d6cd3b2b6
Yitong Wu <wuuuyt72@gmail.com>
2026-04-16 00:37:25 -0400

    feat: Notes section added and languege option enabled

--- src/content/config.ts
+++ src/content/config.ts
@@ -11,4 +11,16 @@ const blog = defineCollection({
   }),
 });
 
-export const collections = { blog };
+const notes = defineCollection({
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    date: z.coerce.date(),
+    description: z.string().optional(),
+    categories: z.array(z.string()).optional().default([]),
+    lang: z.enum(['en', 'zh']),
+    draft: z.boolean().optional().default(false),
+  }),
+});
+
+export const collections = { blog, notes };
